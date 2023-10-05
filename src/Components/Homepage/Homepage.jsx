562a01d612b8a39f5a40bdd3b9f38bed4a789590
TheHolyUshanka <emilm281@gmail.com>
2023-10-05 18:00:14 +0200

    gg

---
+++ src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import "./Homepage.css"
+
+export default function Homepage() {
+  return (
+    <h1>Homepage</h1>
+  )
+}
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Col, Button, Row, Container, Card, Form, Image } from "react-bootstrap";
+
+import "./Login.css"
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+  
+    return (
+        <form>
+            <label>Username:
+            <br></br>
+                <input type="text" />
+            </label>
+            <br></br>
+            <label>Password:
+            <br></br>
+                <input type="password" />
+            </label>
+            <br></br>
+            <input type="submit" value="Login" className='Login'/>
+        </form>
+    );
 }
