36dadbd34e2b704da66f023fc9e746d6afe24c9d
Louisdrejer <118453273+Louisdrejer@users.noreply.github.com>
2023-10-09 15:54:49 +0200

    Homepage

---
+++ src/Components/Homepage/LeftNav.jsx
@@ -0,0 +1,39 @@
+
+import React from 'react'
+import "./Homepage.css"
+export default function LeftNav() {
+  return (
+    <div className="Leftcontainer">
+    <div className="logo_name_left">
+      <div className="logo">logo</div>
+      <div className="name">StackIT</div>
+    </div>
+    <hr style={{ width: "80%", border: "1.8px solid white", marginTop: "10px" }} />
+    <div className="findAnswers">
+      Find Answers
+    </div>
+    <hr style={{ width: "80%", border: "1px solid white", marginTop: "10px" }} />
+    <div className="searchPage">
+      Search Page
+    </div>
+    <div className="activity">
+      Activity
+    </div>
+    <hr style={{ width: "80%", border: "1px solid white", marginTop: "10px" }} />
+    <div className="questions_answers">
+      Questions & Answers
+    </div>
+    <div className="profile">
+      <div className="userLogo">
+        <div className="smallUserLogo"></div>
+      </div>
+      <div className="profileName">
+        ITUCPH
+      </div>
+      <div className="profileEmail">
+        itucph@email.dk
+      </div>
+    </div>
+  </div>
+  )
+}
