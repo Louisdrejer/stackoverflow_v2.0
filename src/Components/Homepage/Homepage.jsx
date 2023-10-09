92691ae4e26e6d64b98e4b77dd9176ecacdd2d07
Louisdrejer <118453273+Louisdrejer@users.noreply.github.com>
2023-10-09 15:51:05 +0200

    homepage

---
+++ src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Homepage.css"
+
+import LeftNav from './LeftNav';
+import SubmitBox from './SubmitBox';
+export default function Homepage() {
+  return (
+    <div className="maincontainer">
+    <LeftNav/>
+    <SubmitBox/>
+  </div>
+  )
+}
