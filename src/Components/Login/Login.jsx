63488c1522f285626e30adab42eaec176d1335e1
TheHolyUshanka <emilm281@gmail.com>
2023-10-12 16:37:17 +0200

    adjustments to login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -9,33 +9,29 @@ export default function Login() {
   
     return (
         <div className="mainContainerLogin">
-            <div className="topBar">
-                <img src={logo} alt="Logo" style={{padding: "16px", maxWidth: "440px"}}/>
-            </div>
-            <div className="mainContainerLoginRow">
-                <div className="formContainer">
-                    <form>
-                        <div className='loginIconDiv'>
-                            <img className="loginIcon" src={profileIcon} alt="Logo" />
+            <img className='LogoLogin' src={logo} alt="Logo"/>
+            <div className="formContainer">
+                <form>
+                    <div className='loginIconDiv'>
+                        <img className="loginIcon" src={profileIcon} alt="Logo" />
                         </div>
-                        <div className='inputField'>
-                            <label className="headLine">Username</label>
-                            <input className="textField" type="text" />
-                        </div>
-                        <div className='inputField'>
-                            <label className="headLine">Password</label>
-                            <input className="textField" type="password" />
-                        </div>
-                        <div className="LinkForgotPasswordDiv">
-                            <Link className="LinkForgotPassword">Forgot Password?</Link>
-                        </div>
-                        <div className='Login'>
-                            <input type="submit" value="Login" className='LoginButton'/>
-                            <label style={{ paddingTop: "8px"}}>or</label>
-                            <Link className="LinkForgotPassword" style={{ paddingTop: "8px"}}>Create New Account</Link>
-                        </div>
-                    </form>
-                </div>
+                    <div className='inputField'>
+                        <label className="headLine">Username</label>
+                        <input className="textField" type="text" />
+                    </div>
+                    <div className='inputField'>
+                        <label className="headLine">Password</label>
+                        <input className="textField" type="password" />
+                    </div>
+                    <div className="LinkForgotPasswordDiv">
+                        <Link className="LinkForgotPassword">Forgot Password?</Link>
+                    </div>
+                    <div className='Login'>
+                        <input type="submit" value="Login" className='LoginButton'/>
+                        <label style={{ padding: "4px"}}>or</label>
+                        <Link className="LinkForgotPassword" style={{ paddingTop: "4px"}}>Create New Account</Link>
+                    </div>
+                </form>
             </div>
         </div>
 
