f1b815c34a7112ca4436aa40ab6d1a7e5fe93a01
haiduri <hssl80@hotmail.com>
2023-11-19 21:35:38 +0100

    I have re-implemented the log in page but the old code is just commented out. Furthermore i have implemented the forgot password page and create new account. I could not include the logo.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import Homepage from './Components/Homepage/Homepage';
-import Login from "./Components/Login/Login"
+// import Login from "./Components/Login/Login"
 import Profile from "./Components/Profile/Profile"
+import {NewUser} from "./Components/NewUser/NewUser";
+import {MyLogin} from "./Components/myLogin/MyLogin";
+import {ForgetPassword} from "./Components/forgotPassword/ForgetPassword";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/homepage" element={<Homepage />}></Route>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/homepage" element={<Homepage/>}></Route>
+                {/*<Route path="/" element={<Login/>}></Route>*/}
+                <Route path="/" element={<MyLogin/>}></Route>
+                <Route path="/profile" element={<Profile/>}></Route>
+                <Route path="/NewUser" element={<NewUser/>}></Route>
+                <Route path="/ForgotPassword" element={<ForgetPassword/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 
-  
+
 }
 
 export default App;
