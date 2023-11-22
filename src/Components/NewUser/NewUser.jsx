12aa344ae3d5b5993da08204bb0658c8e4d3ba92
Xiaofan Sun <135681059+FrancaSun@users.noreply.github.com>
2023-11-22 23:59:07 +0100

    Update NewUser.jsx
    second commit

--- src/Components/NewUser/NewUser.jsx
+++ src/Components/NewUser/NewUser.jsx
@@ -1,37 +1,41 @@
 import React from 'react'
 import "./NewUser.css"
+import {Link} from 'react-router-dom';
+
 
 import logo from '../../img/Logo.png';
 
+
 export function NewUser() {
     return (
-        <>
-            <Link to="/myLogin">
-                <img src={logo} alt="Logo" className="page-logo"/> 
-            </Link>
-            <div className="wrapper">
+    <>
+        <Link to="/myLogin">
+            <img src={logo} alt="Logo" className="page-logo"/> 
+        </Link>
+        <div className="wrapper">
 
-                <form action="">
-                    <h1>Create Your Account</h1>
+            <form action="">
+                <h1>Create Your Account</h1>
 
-                    <div className="input-box">
-                        <input type="text" placeholder="Username" required/>
-                    </div>
+                <div className="input-box">
+                    <input type="text" placeholder="Username" required/>
+                </div>
 
-                    <div className="input-box">
-                        <input type="email" placeholder="Email" required/>
-                    </div>
+                <div className="input-box">
+                    <input type="email" placeholder="Email" required/>
+                </div>
 
-                    <div className="input-box">
-                        <input type="password" placeholder="Password" required/>
-                    </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required/>
+                </div>
 
-                    <div className="input-box">
-                        <input type="Password" placeholder="Repeat Password" required/>
-                    </div>
+                <div className="input-box">
+                    <input type="Password" placeholder="Repeat Password" required/>
+                </div>
 
-                    <button type="submit" className="btn">Create New Account</button>
-                </form>
-            </div>
+                <button type="submit" className="btn">Create New Account</button>
+            </form>
+        </div>
+    </>
     )
 }
