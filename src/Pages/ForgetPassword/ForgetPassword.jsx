64f1a63a7242ba578e10d4d474be9624d5e0a4a4
Louisdrejer <118453273+Louisdrejer@users.noreply.github.com>
2023-11-23 13:10:35 +0100

    Haiders updates

--- src/Pages/ForgetPassword/ForgetPassword.jsx
+++ src/Pages/ForgetPassword/ForgetPassword.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import "./ForgetPassword.css"
-export function ForgetPassword() {
+import './ForgetPassword.css'
+export default function ForgetPassword() {
     return (
         <div className="wrapper">
 
--- src/Pages/Login/Mylogin.jsx
+++ src/Pages/Login/Mylogin.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import "./myLogin.css"
+import './Mylogin.css'
 import {Link} from 'react-router-dom';
 
-export function MyLogin() {
+export default function MyLogin() {
     return (
         <>
             <div className="wrapper">
