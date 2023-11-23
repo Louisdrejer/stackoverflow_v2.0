a8d5f1b975c07c634a51bf150d992510e5304078
Emil <emvo@itu.dk>
2023-11-23 18:56:45 +0100

    Added server

--- src/Pages/Login/Login2.jsx
+++ src/Pages/Login/Login2.jsx
@@ -1,29 +1,48 @@
 import React from 'react'
+import { useState } from 'react';
 import "./Login.css"
 import {Link} from 'react-router-dom';
-
+import { useNavigate } from 'react-router';
 import logo from '../../img/Logo.png';
 import profileIcon from '../../img/loginIcon.png';
+//import Parse from 'parse';
 
 export default function MyLogin() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+   // const handleLogin = async (username, pw) => {
+   //     var user = Parse.User.logIn(username, pw).then(function(user) {
+  //        navigate('/Profile');
+   //     }).catch(function(error) {
+  //        console.log("Error: " + error.code + " " + error.message);
+  //      });
+   //   };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+   //     await handleLogin(username, password);
+      };
+
     return (
         <>
             <Link to="/"><img src={logo} alt="Logo" className="page-logo" /></Link>
             <div className="wrapper9">
 
-                <form action="">
+                <form onSubmit={onSubmit}>
                     <img src={profileIcon} alt="Login Icon" className="login-icon" /> 
                     <div className="input-box">
-                        <input type="text" placeholder="Username" required/>
+                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required/>
                         <i className='bx bxs-user'></i>
                     </div>
                     <div className="input-box">
-                        <input type="Password" placeholder="Password" required/>
+                        <input type="Password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
                         <i className='bx bxs-lock-alt'></i>
                     </div>
                     <div className="remember-forgot">
                     <label className="checkbox-label">
-                        <input className="checkBox" type="checkbox" />
+                        <input className="checkBox" type="checkbox"  />
                         <span className="custom-checkbox"></span>
                             Remember me
                     </label>
