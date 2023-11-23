c4499d241d8ba5b8f1de00f592eb64cc6e6f7aaf
Louisdrejer <118453273+Louisdrejer@users.noreply.github.com>
2023-11-23 15:05:59 +0100

    updates

--- src/Outerlayout/Outerlayout.jsx
+++ src/Outerlayout/Outerlayout.jsx
@@ -3,11 +3,9 @@ import { Outlet } from "react-router-dom"
 function reducedLayout() {
   return (
     <>
-      <div className="container1">
-        <div className="container2">
+ 
             <Outlet context={{ hello: "world" }} />
-        </div>
-      </div>
+
     </>
   )
 }
