7f5f50aa0b469ca07a40345f80662cf57955e05d
Emil <emvo@itu.dk>
2023-12-07 13:57:20 +0100

    topics

--- src/Components/TopicDropDown.jsx
+++ src/Components/TopicDropDown.jsx
@@ -13,19 +13,19 @@ export default function TopicDropDown({ onTopicChange }) {
 
     return (
         <div className="SP1dropdown">
-            <span>{selectedTopic}</span>
-            <div className="SP1dropdown-content">
-                <p onClick={() => handleTopicChange('TOPIC #1')} style={{ cursor: 'pointer' }}>
-                    TOPIC #1
-                </p>
-                <p onClick={() => handleTopicChange('TOPIC #2')} style={{ cursor: 'pointer' }}>
-                    TOPIC #2
-                </p>
-                <p onClick={() => handleTopicChange('TOPIC #3')} style={{ cursor: 'pointer' }}>
-                    TOPIC #3
-                </p>
-            </div>
-            <div className="SP1arrow-down"></div>
+        <span>{selectedTopic}</span>
+        <div className="SP1dropdown-content">
+        <p onClick={() => handleTopicChange('DATA TYPES')} style={{ cursor: 'pointer' }}>
+            DATA TYPES
+        </p>
+        <p onClick={() => handleTopicChange('LISTS')} style={{ cursor: 'pointer' }}>
+            LISTS
+        </p>
+        <p onClick={() => handleTopicChange('BACKEND')} style={{ cursor: 'pointer' }}>
+            BACKEND
+        </p>
         </div>
+        <div className="SP1arrow-down"></div>
+    </div>
     );
 }
