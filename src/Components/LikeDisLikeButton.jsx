bf60beaf547bb51bbcff4066f135746727f7129f
TheHolyUshanka <emilm281@gmail.com>
2023-12-07 12:14:07 +0100

    db like dislike state working

--- src/Components/LikeDisLikeButton.jsx
+++ src/Components/LikeDisLikeButton.jsx
@@ -3,67 +3,88 @@
 import React, { useState } from 'react';
 import './LikeDislike.css';
 import { BiSolidLike, BiSolidDislike } from 'react-icons/bi';
-import { updateLikesInDatabase, updateDislikesInDatabase } from '../Scripts/Database';
+import { updateLikeDislikeDB } from '../Scripts/Database';
 
 const LikeDislikeButtons = (props) => {
-  const [dislikes, setDislikes] = useState(props.disLikes);
-  const [likes, setLikes] = useState(props.likes);
-  const [likeClicked, setLikeClicked] = useState(false);
-  const [dislikeClicked, setDislikeClicked] = useState(false);
+  const dislikes = props.disLikes
+  const likes = props.likes
+  const [like, setLike] = useState(false);
+  const [dislike, setDislike] = useState(false);
 
-  const handleLike = async () => {
-    if (!likeClicked) {
-      setLikes(likes + 1);
-      if (dislikeClicked) {
-        setDislikes(dislikes - 1);
-        setDislikeClicked(false);
-        await updateDislikesInDatabase(props.objectId, dislikes - 1);
-      }
-      await updateLikesInDatabase(props.objectId, likes + 1);
-    } else {
-      setLikes(likes - 1);
-      await updateLikesInDatabase(props.objectId, likes - 1);
+  // const handleLike = async () => {
+  //   if (!likeClicked) {
+  //     setLikes(likes + 1);
+  //     if (dislikeClicked) {
+  //       setDislikes(dislikes - 1);
+  //       setDislikeClicked(false);
+  //       await updateDislikesInDatabase(props.objectId, dislikes - 1);
+  //     }
+  //     await updateLikesInDatabase(props.objectId, likes + 1);
+  //   } else {
+  //     setLikes(likes - 1);
+  //     await updateLikesInDatabase(props.objectId, likes - 1);
+  //   }
+  //   setLikeClicked(!likeClicked);
+  // };
+
+  const handleLike = () => {
+    if (!dislike) {
+      setLike(!like);
+      
+      //increment like in DB if false, else decrement
+      updateLikeDislikeDB(props.objectId, "Likes", !like)
+
     }
-    setLikeClicked(!likeClicked);
   };
 
-  const handleDislike = async () => {
-    if (!dislikeClicked) {
-      setDislikes(dislikes + 1);
-      if (likeClicked) {
-        setLikes(likes - 1);
-        setLikeClicked(false);
-        await updateLikesInDatabase(props.objectId, likes - 1);
-      }
-      await updateDislikesInDatabase(props.objectId, dislikes + 1);
-    } else {
-      setDislikes(dislikes - 1);
-      await updateDislikesInDatabase(props.objectId, dislikes - 1);
+  // const handleDislike = async () => {
+  //   if (!dislikeClicked) {
+  //     setDislikes(dislikes + 1);
+  //     if (likeClicked) {
+  //       setLikes(likes - 1);
+  //       setLikeClicked(false);
+  //       await updateLikesInDatabase(props.objectId, likes - 1);
+  //     }
+  //     await updateDislikesInDatabase(props.objectId, dislikes + 1);
+  //   } else {
+  //     setDislikes(dislikes - 1);
+  //     await updateDislikesInDatabase(props.objectId, dislikes - 1);
+  //   }
+  //   setDislikeClicked(!dislikeClicked);
+  // };
+
+
+  const handleDislike = () => {
+    if (!like) {
+      setDislike(!dislike);
+
+      //add like to DB
+      updateLikeDislikeDB(props.objectId, "Dislikes", !dislike)
+
     }
-    setDislikeClicked(!dislikeClicked);
   };
 
   return (
     <div className='LikeDislikeButton'>
       <div className='likeButton'>
         <div
-          className={`likeIcon ${likeClicked ? 'clicked2' : ''}`}
+          className={`likeIcon ${like ? 'clicked2' : ''}`}
           onClick={handleLike}
         >
           <BiSolidLike />
         </div>
-        <div className='likeCount'>{likes}</div>
+        <div className='likeCount'>{Number(like) + likes}</div>
       </div>
       <div style={{ fontSize: '14px' }}>|</div>
       <div className='DislikeButton'>
         <div
-          className={`DislikeIcon ${dislikeClicked ? 'clicked2' : ''}`}
+          className={`DislikeIcon ${dislike ? 'clicked2' : ''}`}
           style={{ transform: 'scaleX(-1)' }}
           onClick={handleDislike}
         >
           <BiSolidDislike />
         </div>
-        <div className='DislikeCount'>{dislikes}</div>
+        <div className='DislikeCount'>{Number(dislike) + dislikes}</div>
       </div>
     </div>
   );
