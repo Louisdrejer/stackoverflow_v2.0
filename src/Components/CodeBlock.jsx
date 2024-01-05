8a9515eaddb78884b2f2a2bef3d549e2b2bb0777
Louisdrejer <118453273+Louisdrejer@users.noreply.github.com>
2024-01-06 00:03:58 +0100

    updates

--- src/Components/CodeBlock.jsx
+++ src/Components/CodeBlock.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CodeBlock.css'; // Import the CSS file for styling
 
 const CodeBlock = ({ code }) => {
+  const [isClicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!isClicked);
+  };
+
+  const code1Style = {
+    minWidth: 'calc(100% - 5px)',
+    paddingTop: '5px',
+    paddingLeft: '5px',
+    paddingBottom: '-5px',
+    borderRadius: '5px',
+    height: '100px',
+    marginBottom: '10px',
+    overflowY: 'auto',
+    color: 'white',
+  };
+
+  const code2Style = {
+    paddingTop: '5px',
+    paddingLeft: '5px',
+    paddingBottom: '-5px',
+    borderRadius: '5px',
+    marginBottom: '10px',
+    overflowY: 'auto',
+    color: 'white',
+    height: 'max-content',
+  };
+
+  const appliedStyle = isClicked ? code2Style : code1Style;
+
   return (
-    <pre className="code">
+    <pre className="code" style={appliedStyle} onClick={handleClick}>
       <code>{code}</code>
     </pre>
   );
--- src/Components/SmallCodeBlock.jsx
+++ src/Components/SmallCodeBlock.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CodeBlock.css'; // Import the CSS file for styling
 
 const CodeBlock = ({ code }) => {
+  const [isClicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!isClicked);
+  };
+
+  const code1Style = {
+    paddingTop: '5px',
+    paddingLeft: '5px',
+    paddingBottom: '-5px',
+    borderRadius: '5px',
+    height: '100px',
+    marginBottom: '10px',
+    overflowY: 'auto',
+    width: '100%',
+    color: 'white',
+  };
+
+  const code2Style = {
+    paddingTop: '5px',
+    paddingLeft: '5px',
+    paddingBottom: '-5px',
+    borderRadius: '5px',
+    marginBottom: '10px',
+    overflowY: 'auto',
+    minWidth: '100%',
+    color: 'white',
+    minHeight: '100px',
+    height: 'max-content',
+  };
+
+  const appliedStyle = isClicked ? code2Style : code1Style;
+
   return (
-    <pre className="smallCode">
+    <pre className="smallCode" style={appliedStyle} onClick={handleClick}>
       <code>{code}</code>
     </pre>
   );
