c7eb751c45672e757c34b3536a0c5a8bd10fd737
Louisdrejer <118453273+Louisdrejer@users.noreply.github.com>
2024-01-09 15:35:39 +0100

    updates

--- src/Components/ConfirmationModal.jsx
+++ src/Components/ConfirmationModal.jsx
@@ -1,11 +1,14 @@
+// ConfirmationModal.js
 import React from 'react';
 
 const ConfirmationModal = ({ onConfirm, onCancel }) => {
   return (
-    <div className="confirmation-modal">
-      <p>Are you sure you want to delete this?</p>
-      <button onClick={onConfirm}>Yes</button>
-      <button onClick={onCancel}>No</button>
+    <div className="confirmation-modal-overlay">
+      <div className="confirmation-modal">
+        <p>Are you sure you want to delete this comment?</p>
+        <button onClick={onConfirm}>Yes</button>
+        <button onClick={onCancel}>No</button>
+      </div>
     </div>
   );
 };
