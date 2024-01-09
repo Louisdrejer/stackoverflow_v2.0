24c6391628ebd816932127c1f678d652c4322b2c
Louisdrejer <118453273+Louisdrejer@users.noreply.github.com>
2024-01-09 15:44:53 +0100

    Alert

--- src/Components/MyCommentBox.jsx
+++ src/Components/MyCommentBox.jsx
@@ -1,3 +1,94 @@
+
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import CodeBlock from './CodeBlock';
+import SmallCodeBlock from './SmallCodeBlock';
+import './CodeBlock.css';
+import LikeDislikeButtonNotclick from './LikeDisLikeButtonNotClick';
+import { deleteCommitsById } from '../Scripts/Database';
+import { RiDeleteBin2Line } from "react-icons/ri";
+import ConfirmationModal from './ConfirmationModal';
+
+export default function MycommentBox(props) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const getColorForTag = (tag) => {
+    if (tag === "PYTHON" || tag === "JAVA" || tag === "JAVASCRIPT") {
+      return "rgb(255, 219, 183)";
+    } else if (tag === "BEGINNER" || tag === "INTERMEDIATE" || tag === "EXPERT") {
+      return "rgb(254, 207, 228)";
+    } else {
+      return "rgb(154, 218, 112)";
+    }
+  };
+
+  const handleDeleteClick = () => {
+    setShowConfirmation(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    try {
+      await deleteCommitsById(props.objectId);
+      props.setUpdate(props.update + 1);
+      setShowConfirmation(false);
+    } catch (error) {
+      console.error("Error deleting comment:", error);
+    }
+  };
+
+  const handleCancelDelete = () => {
+    setShowConfirmation(false);
+  };
+
+  const goToAnswerBoxPage = () => {
+    navigate(`../Answers/`, { state: { pid: props.objectId, username: props.name, title: props.title, text: props.text, tags: props.tags } });
+  };
+
+  return (
+    <div className="newQustionBox" style={{ background: "rgb(53, 54, 58)", borderColor: "rgb(53, 54, 58)", width: "90%", marginLeft: "5%" }}>
+      <div className="newQustionHeader">
+        <div className="userQ">
+          <div className="smallUserLogo2"></div>
+          <div className="Headline" onClick={goToAnswerBoxPage}>{props.title}</div>
+        </div>
+        {location.pathname === '/OtherProfile' ? (
+          <div></div>
+        ) : (
+          <div className="DeleteContainer" onClick={handleDeleteClick}style={{cursor: 'pointer'}}>
+            <RiDeleteBin2Line />
+          </div>
+        )}
+      </div>
+      <div className="newQuestionBody">
+        {location.pathname === '/Profile' || location.pathname === '/profile' || location.pathname === '/OtherProfile' ? (
+          <SmallCodeBlock code={props.text} />
+        ) : (
+          <CodeBlock code={props.text} />
+        )}
+      </div>
+      <div className="newQuestionTags">
+        {props.tags.map((tag, index) => (
+          <div key={index} style={{ backgroundColor: getColorForTag(tag), color: "black", border: "0px" }} className="NQtag">
+            {tag}
+          </div>
+        ))}
+        <div className="likeDislike-button-notCLick-container ">
+          <LikeDislikeButtonNotclick likes={props.likes} dislikes={props.dislikes} />
+        </div>
+      </div>
+
+      {showConfirmation && <ConfirmationModal onConfirm={handleConfirmDelete} onCancel={handleCancelDelete} />}
+    </div>
+  );
+}
+      
+      
+      
+{/**
+      
 import React from 'react';
 import CodeBlock from './CodeBlock'; 
 import SmallCodeBlock from './SmallCodeBlock'
@@ -80,3 +171,5 @@ export default function MycommentBox(props) {
     </div>
   );
         }
+
+ */}
